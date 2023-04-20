bbde036a9531005d08e37cf7d3e030fe928f2d1b
NicolasBronzini <104471107+NicolasBronzini@users.noreply.github.com>
2023-04-20 12:39:10 -0300

    Interface

--- interface/interfaz_grafica.py
+++ interface/interfaz_grafica.py
@@ -8,12 +8,20 @@ class InterfazGrafica(tk.Frame):
         self.crear_widgets()
 
     def crear_widgets(self):
-        self.etiqueta = tk.Label(self, text="Hola, esta es una interfaz gráfica básica.")
+        self.etiqueta = tk.Label(self, text="¡Bienvenido al Juego!")
         self.etiqueta.pack(side="top")
 
-        self.boton = tk.Button(self, text="Salir", fg="red",
-                              command=self.master.destroy)
-        self.boton.pack(side="bottom")
+        self.boton_jugar = tk.Button(self, text="Jugar", fg="green",
+                                     command=self.abrir_juego)
+        self.boton_jugar.pack(side="left")
+
+        self.boton_salir = tk.Button(self, text="Salir", fg="red",
+                                     command=self.master.destroy)
+        self.boton_salir.pack(side="right")
+
+    def abrir_juego(self):
+        # Aquí puedes llamar a la función que abre el juego
+        pass
 
 if __name__ == "__main__":
     root = tk.Tk()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 from BD.crear_tabla import crear_tabla
 from interface.interfaz_grafica import InterfazGrafica
+from BD.preguntas import preguntas
 
 # Creacion de tabla en Db
 crear_tabla()
